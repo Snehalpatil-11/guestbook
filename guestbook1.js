5d43ce0f4d1e9b0bc9ba42c48bb595f52ba7021e
Patilsnehal11 <snehalpatil.opeinfotech@gmail.com>
2024-06-01 18:34:01 +0530

    guestbook

---
+++ guestbook1.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const commentForm = document.getElementById('comment-form');
+    const commentInput = document.getElementById('comment');
+    const commentsContainer = document.getElementById('comments');
+
+    commentForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const commentText = commentInput.value.trim();
+
+        if (commentText !== '') {
+            const commentDiv = document.createElement('Div');
+            commentDiv.classList.add('comment');
+            commentDiv.textContent = commentText;
+            commentsContainer.appendChild(commentDiv);
+            commentInput.value = '';
+        }
+    });
+});
+
+
